bd4aa33ea70186b4466d155b7bcdf0c3ec8a25e7
klagiewczyk <krzylery@gmail.com>
2025-09-15 07:38:09 +0000

    New style intro

--- src/src/Ui/Intro.jsx
+++ src/src/Ui/Intro.jsx
@@ -1,4 +1,6 @@
+import { Canvas } from '@react-three/fiber'
 import { useCallback, useEffect, useRef, useState } from 'react'
+import { Model } from '../Objects/HelperObjects/Model.jsx'
 
 export function Intro({ children }) {
 	const [showChildren, setShowChildren] = useState(false)
@@ -31,23 +33,20 @@ export function Intro({ children }) {
 			{showChildren && children}
 
 			{!hidden && (
-				<div className={`text-center ${isFadingOut ? 'fade-out' : ''}`}>
-					<div className="p-5 flex flex-col gap-5 items-center justify-center h-screen bg-black text-white">
+				<div className={`p-5 flex flex-row gap-10 items-center justify-center h-screen bg-black text-white ${isFadingOut ? 'fade-out' : ''}`}>
+					<div className="flex-1 flex flex-col gap-5 items-center text-center">
 						<h1 className="text-5xl">Drive Thru</h1>
-						<p className="">Press Enter, Space or click to start</p>
-
+						{/* <p>Press Enter, Space or click to start</p> */}
 						<button className="border-solid text-white bg-red-500 px-5 rounded-full" onClick={start} aria-label="Start">
 							Start
 						</button>
-
-						<div className="">
-							<p>Controls:</p>
-							<ul>
-								<li>Move: W / S or Arrow Keys</li>
-								<li>Steer: A / D or Arrow Keys</li>
-								<li>Enter or Space: Start</li>
-							</ul>
-						</div>
+					</div>
+					<div className="flex-1 flex items-center justify-center">
+						<Canvas className="w-100 h-200 rounded-lg" shadows camera={{ position: [5, 2, -5], fov: 70}}>
+							<ambientLight intensity={0.5} />
+							<directionalLight position={[5, 30, 5]} castShadow/>
+							<Model name={"truck01"} type={"car"} scale={[7,7,7]}/>
+						</Canvas>
 					</div>
 				</div>
 			)}
