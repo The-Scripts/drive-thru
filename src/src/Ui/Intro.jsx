496ff257490d469c3dfea4b6373a4df894cc8925
klagiewczyk <krzylery@gmail.com>
2025-09-15 08:05:23 +0000

    Intro - fix

--- src/src/Ui/Intro.jsx
+++ src/src/Ui/Intro.jsx
@@ -33,22 +33,22 @@ export function Intro({ children }) {
 			{showChildren && children}
 
 			{!hidden && (
-				<div className={`p-5 flex flex-row gap-10 items-center justify-center h-screen bg-black text-white ${isFadingOut ? 'fade-out' : ''}`}>
-					<div className="flex-1 flex flex-col gap-5 items-center text-center">
-						<h1 className="text-5xl">Drive Thru</h1>
-						{/* <p>Press Enter, Space or click to start</p> */}
-						<button className="border-solid text-white bg-red-500 px-5 rounded-full" onClick={start} aria-label="Start">
-							Start
-						</button>
-					</div>
-					<div className="flex-1 flex items-center justify-center">
-						<Canvas className="w-100 h-200 rounded-lg" shadows camera={{ position: [5, 2, -5], fov: 70}}>
-							<ambientLight intensity={0.5} />
-							<directionalLight position={[5, 30, 5]} castShadow/>
-							<Model name={"truck01"} type={"car"} scale={[7,7,7]}/>
-						</Canvas>
-					</div>
-				</div>
+				   <div className={`p-5 flex flex-row items-center justify-center h-screen bg-black text-white ${isFadingOut ? 'fade-out' : ''}`}>
+					   <div className=" flex-1 flex flex-col gap-5 items-center justify-center text-center self-center mb-100">
+						   <h1 className="text-5xl mt-2 mb-8 w-full text-center">Drive Thru</h1>
+						   {/* <p>Press Enter, Space or click to start</p> */}
+						   <button className="border-solid text-white bg-red-500 px-20 py-4 text-2xl rounded-full hover:bg-red-600 transition-all duration-200" onClick={start} aria-label="Start">
+							   Start
+						   </button>
+					   </div>
+					   <div className="flex-1 flex items-center justify-center">
+						   <Canvas className="w-full h-full rounded-lg" shadows camera={{ position: [5, 2, -5], fov: 70}} style={{height: '70vh', width: '100%'}}>
+							   <ambientLight intensity={0.5} />
+							   <directionalLight position={[5, 30, 5]} castShadow/>
+							   <Model name={"truck01"} type={"car"} scale={[6,6,6]}/>
+						   </Canvas>
+					   </div>
+				   </div>
 			)}
 		</>
 	)
