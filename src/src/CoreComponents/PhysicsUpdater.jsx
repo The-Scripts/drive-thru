242816c0540b93e24ee36a235a47f006da8ef51c
Cushy <147443259+NoKrecik@users.noreply.github.com>
2025-09-21 11:27:36 +0200

    Set physics world to fixed 60 fps

--- src/src/CoreComponents/PhysicsUpdater.jsx
+++ src/src/CoreComponents/PhysicsUpdater.jsx
@@ -4,9 +4,18 @@ import { usePhysicsWorld } from './PhysicsWorldContext';
 export const PhysicsUpdater = () => {
     const world = usePhysicsWorld();
 
-    useFrame(() => {
-        if (world) {
+    let stepsPassed = 0;
+    const fixedTimeStep = 1 / 60;
+
+    useFrame((_, delta) => {
+        if (!world) return
+
+        delta = Math.min(delta, 0.05);
+        stepsPassed += delta
+
+        while (stepsPassed >= fixedTimeStep) {
             world.step();
+            stepsPassed -= fixedTimeStep;
         }
     });
 
