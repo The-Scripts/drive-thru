be5f4cbf6c200caecf922548b2b5cc079b07ac04
Łukasz Stępień <lukaszatepien@gmail.com>
2025-09-22 10:04:21 +0200

    Java

---
+++ src/Drive Thru Java/src/test/java/com/thescripts/drive_thru_java/DriveThruJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thescripts.drive_thru_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveThruJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
