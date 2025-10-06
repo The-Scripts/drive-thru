f7ea14a0abb8d228449ac49c9da095700e18360c
Patryk-Ruzicki <arcysanta@gmail.com>
2025-10-06 09:01:04 +0200

    click on object ball (color to green)

--- src/src/Objects/BaseObjects.jsx
+++ src/src/Objects/BaseObjects.jsx
@@ -1,19 +1,29 @@
 import { useRapier } from "../CoreComponents/RapierContext.jsx";
 import { usePhysicsWorld } from "../CoreComponents/PhysicsWorldContext.jsx";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
 
-export const Platform = ({ position = [0, 0, 0], size = [10, 1, 10], color = "green" }) => {
+export const Platform = ({
+    position = [0, 0, 0],
+    size = [10, 1, 10],
+    color = "green",
+}) => {
     const RAPIER = useRapier();
     const world = usePhysicsWorld();
 
     useEffect(() => {
         if (!RAPIER || !world) return;
 
-        const rigidBodyDesc = RAPIER.RigidBodyDesc.fixed().setTranslation(...position);
+        const rigidBodyDesc = RAPIER.RigidBodyDesc.fixed().setTranslation(
+            ...position
+        );
         const rigidBody = world.createRigidBody(rigidBodyDesc);
 
-        const colliderDesc = RAPIER.ColliderDesc.cuboid(size[0]/2, size[1]/2, size[2]/2);
+        const colliderDesc = RAPIER.ColliderDesc.cuboid(
+            size[0] / 2,
+            size[1] / 2,
+            size[2] / 2
+        );
         const collider = world.createCollider(colliderDesc, rigidBody);
     }, [RAPIER, world]);
 
@@ -22,20 +32,29 @@ export const Platform = ({ position = [0, 0, 0], size = [10, 1, 10], color = "gr
             <boxGeometry args={size} />
             <meshStandardMaterial color={color} />
         </mesh>
-    )
-}
+    );
+};
 
-export const Ball = ({ position = [0, 0, 0], radius = [0.5], color = "blue", restitution = 0.8}) => {
+export const Ball = ({
+    position = [0, 0, 0],
+    radius = 0.5,
+    color = "blue",
+    restitution = 0.8,
+}) => {
     const RAPIER = useRapier();
     const world = usePhysicsWorld();
 
     const meshRef = useRef();
     const rigidBodyRef = useRef();
+    const [currentColor, setCurrentColor] = useState(color);
+    const [inCart, setInCart] = useState(false);
 
     useEffect(() => {
         if (!RAPIER || !world) return;
 
-        const rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic().setTranslation(...position);
+        const rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic().setTranslation(
+            ...position
+        );
         const rigidBody = world.createRigidBody(rigidBodyDesc);
         rigidBodyRef.current = rigidBody;
 
@@ -56,10 +75,20 @@ export const Ball = ({ position = [0, 0, 0], radius = [0.5], color = "blue", res
         }
     });
 
+    const handleClick = () => {
+        setCurrentColor("green");
+        setInCart(true);
+    };
+
     return (
-        <mesh ref={meshRef} position={position} castShadow>
-            <sphereGeometry args={radius} />
-            <meshStandardMaterial color={color}/>
+        <mesh
+            ref={meshRef}
+            position={position}
+            castShadow
+            onClick={handleClick}
+        >
+            <sphereGeometry args={[radius]} />
+            <meshStandardMaterial color={currentColor} />
         </mesh>
-    )
-}
+    );
+};
